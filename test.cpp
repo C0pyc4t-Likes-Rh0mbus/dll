1c7168aeb3c80754f2ceddb7d704273e75e8908f
차승주 <chaseungju@chaseungjuui-MacBook-Pro.local>
2018-10-06 10:51:36 +0900

    dll

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include "stdio.h"
+#include "windows.h"
+
+#paragma comment(lib, "kernel32.dll")
+
+DWORD WINAPI ThreadProc(LPVOID lParam)
+{
+	MessageBox(NULL,"hooking Success!","alarm",MB_OK);
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	HANDLE hThread = NULL;
+
+	switch(fdwReason)
+	{
+		case DLL_PROCESS_ATTACH:
+			hThread = CreateThread(NULL, 0, ThreadProc, NULL, 0, NULL);
+			CloseHandle(hThread);
+			break;
+	}
+	return TRUE;
+}
